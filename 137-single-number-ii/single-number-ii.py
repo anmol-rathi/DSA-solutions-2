6ddb22f7dd3f3452c94183826f9cae4d3687afd4
anmol-rathi <anmolrathi0804@gmail.com>
2026-09-07 16:59:17 +0530

    Time: 41 ms (11.34%) | Memory: 20.6 MB (46.95%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,9 +1,27 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(1,len(nums),3):
-            if nums[i]!=nums[i-1]:
-                return nums[i-1]
-        return nums[len(nums)-1]
+        n=len(nums)
+        res=0
+        for bits in range(32):
+            count=0
+            for i in nums:
+                if i<0:
+                    i=i& (2**32 -1)
+                if i&(1<<bits):
+                    count+=1
+            if count%3!=0:
+                res=res|(1<<bits)
+        if res>=2**31:
+            res-=2**32
+        return res
+
+                
+
+        # nums.sort()
+        # for i in range(1,len(nums),3):
+        #     if nums[i]!=nums[i-1]:
+        #         return nums[i-1]
+        # return nums[len(nums)-1] 
+        # # it is o(nlogn+n/3) better than (32n solution)
      
         

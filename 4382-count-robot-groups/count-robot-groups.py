87a856af81602f5b61665f59e63e06ff98f465ae
anmol-rathi <anmolrathi0804@gmail.com>
2026-09-09 11:18:07 +0530

    Time: 1801 ms (5.03%) | Memory: 38.3 MB (81.05%) - LeetSync

---
+++ 4382-count-robot-groups/count-robot-groups.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countGroups(self, position: list[int], speed: list[int], distance: int) -> int:
+        n=len(position)
+        
+        num=0
+        while num<n:
+            if num+1!=n and position[num+1]-position[num]<=distance:
+                position.pop(num)
+                speed.pop(num)
+                n-=1
+            else:
+                num+=1
+        res=0
+        speedmin=speed[n-1]
+        print(position,speed)
+        for i in range(n-1,-1,-1):
+            if i-1>=0:
+                if speed[i-1]>speedmin:
+                    res+=1
+                else:
+                    speedmin=speed[i-1]
+        return n-res
+            
+                
+        

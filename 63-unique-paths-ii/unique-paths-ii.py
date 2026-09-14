14f9817330e5bed27f734a4901d11bfae6b2bab5
anmol-rathi <anmolrathi0804@gmail.com>
2026-09-14 16:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.26%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -4,19 +4,36 @@ class Solution:
         n=len(obstacleGrid[0])
         if obstacleGrid[0][0]==1 or obstacleGrid[m-1][n-1]==1:
             return 0
-        dp=[[-1]*n for _ in range(m)]
-        def f(i,j):
-            if i==0 and j==0:
-                return 1
-            if i<0 or j<0:
-                return 0
-            if obstacleGrid[i][j]==1:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            up=f(i-1,j)
-            left=f(i,j-1)
-            dp[i][j]=up+left
-            return up+left
-        return f(m-1,n-1)
+        dp=[[0]*n for _ in range(m)]
+        dp[0][0]=1
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    continue
+                if obstacleGrid[i][j]==1:
+                    continue
+                if i-1>=0:
+                    left=dp[i-1][j]
+                else:
+                    left=0
+                if j-1>=0:
+                    up=dp[i][j-1]
+                else:
+                    up=0
+                dp[i][j]=left+up
+        return dp[m-1][n-1]
+        # def f(i,j):
+        #     if i==0 and j==0:
+        #         return 1
+        #     if i<0 or j<0:
+        #         return 0
+        #     if obstacleGrid[i][j]==1:
+        #         return 0
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     up=f(i-1,j)
+        #     left=f(i,j-1)
+        #     dp[i][j]=up+left
+        #     return up+left
+        # return f(m-1,n-1)
         
